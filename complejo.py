9e5bdae26cd211b6d39b1fc4f3131f4af47b96f6
gveloz <g_veloz@espoch.edu.ec>
2021-07-26 15:42:22 -0500

    Creación de objeto complejo

---
+++ complejo.py
@@ -0,0 +1,11 @@
+## programa que define una clase para un número complejo.
+
+class Complejo:
+    r=0
+    i=0
+    def __init__(self, realpart,imagpart):
+        self.r=realpart
+        self.i=imagpart
+       
+
+Complejo oComplejo(2,4)
